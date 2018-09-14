c201b7a536b8b1742b4d4e1ee0e531ef896f28bd
Erik <elin7780@bths.edu>
2018-09-14 18:52:33 -0400

    Initial skeleton

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const path = require('path')
+const SRC_DIR = path.join(__dirname, '/src')
+const DIST_DIR = path.join(__dirname, '/dist')
+
+module.exports = {
+  entry: [`${SRC_DIR}/index.jsx`],
+  output: {
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js[x]?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    ]
+  }
+}
