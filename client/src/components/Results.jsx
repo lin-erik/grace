14c69af55a3641f7869945e0ed1495320361bec0
Erik <elin7780@bths.edu>
2018-09-15 12:29:03 -0400

    Initial rendering of suggestions on page

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Grid, Card, Image } from 'semantic-ui-react'
+
 import Navbar from './Navbar.jsx'
 
 class Results extends Component {
@@ -8,7 +10,42 @@ class Results extends Component {
   }
 
   render() {
-    return <Navbar setImageRecs={this.props.setImageRecs} />
+    return (
+      <div>
+        <Navbar setImageRecs={this.props.setImageRecs} />
+
+        <Grid columns={2} style={{marginTop: '10%'}}>
+          <Grid.Column width={6}>
+            <div style={{marginLeft: '15%', width: '15%', height: '50px', border: '1px solid black'}}>
+              Test
+            </div>
+          </Grid.Column>
+
+          <Grid.Column>
+            <Grid columns={3}>
+                {this.props.imageRecs.map((rec, ind) => (
+                  <Grid.Column key={ind}>
+                    <Card>
+                      <Image src={rec.imageUrl} />
+                      <Card.Content>
+                        <Card.Header>{rec.name}</Card.Header>
+                        <Card.Meta>
+                          <span>{rec.brandName}</span>
+                        </Card.Meta>
+                      </Card.Content>
+                      <Card.Content extra>
+                        <a href={rec.url}>
+                          ${rec.price}
+                        </a>
+                      </Card.Content>
+                    </Card>
+                  </Grid.Column>
+                ))}
+            </Grid>
+          </Grid.Column>
+        </Grid>
+      </div>
+    )
   }
 }
 
